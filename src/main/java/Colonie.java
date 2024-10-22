5627a553071020481d4f444463897385b357dca3
thmsgo18 <134527237+thmsgo18@users.noreply.github.com>
2024-10-22 18:57:53 +0200

    update Colonie.java

--- src/main/java/Colonie.java
+++ src/main/java/Colonie.java
@@ -1,38 +1,46 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class Colonie {
     private String nom;
-    private List<Ressource> ressourceDeLaColonie;
-    private List<Colon> Lescolons;
-    public Colonie(String nom, List<Ressource> ressourceDeLaColonie) {
+    private List<Ressource> ressourcesColonie;
+    private List<Colon> colons;
+    private int affectation;
+
+    public Colonie(String nom) {
         this.nom = nom;
-        this.ressourceDeLaColonie = ressourceDeLaColonie;
-        this.Lescolons = new ArrayList<Colon>(26);
+        this.ressourcesColonie = new ArrayList<Ressource>(26);
+        this.colons = new ArrayList<Colon>(26);
     }
     public String getNom() {
         return nom;
     }
 
-    public List<Ressource> getRessourceDeLaColonie() {
-        return ressourceDeLaColonie;
+    public List<Ressource> getRessourcesColonie() {
+        return ressourcesColonie;
     }
 
-    public List<Colon> getLescolons() {
-        return Lescolons;
+    public List<Colon> getColons() {
+        return colons;
     }
 
     public void setNom(String nom) {
         this.nom = nom;
     }
 
-    public void setRessourceDeLaColonie(List<Ressource> ressourceDeLaColonie) {
-        this.ressourceDeLaColonie = ressourceDeLaColonie;
+    public void setRessourcesColonie(List<Ressource> ressourceDeLaColonie) {
+        this.ressourcesColonie = ressourceDeLaColonie;
     }
 
-    public void setLescolons(List<Colon> Lescolons) {
-        this.Lescolons = Lescolons;
+    public void setColons(List<Colon> colons) {
+        this.colons = colons;
+    }
+
+    public int getAffectation() {
+        return affectation;
+    }
+    public void setAffectation(int affectation) {
+        this.affectation = affectation;
     }
 
 }
